2d73bb593eb6edc4e9dc72663ec17f4a54e0655c
unknown <Jonathan@.(none)>
2009-11-12 13:06:05 -0700

    woops

---
+++ db/migrate/20091112195623_create_stories.rb
@@ -0,0 +1,13 @@
+class CreateStories < ActiveRecord::Migration
+  def self.up
+    create_table :stories do |t|
+      t.text :body
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :stories
+  end
+end
