bd3a13545def61198acc31c60cf50bb67ea2491e
unknown <Jonathan@.(none)>
2009-11-12 14:01:46 -0700

    atom

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -18,7 +18,7 @@ class StoriesController < ApplicationController
     
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @story }
+      # format.xml  { render :xml => @story }
       format.atom { render :layout => false }
     end
   end
