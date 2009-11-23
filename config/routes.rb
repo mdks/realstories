09b84b1c66110a95c18db83c8c0ee101bb0ceb4c
unknown <Jonathan@.(none)>
2009-11-23 00:38:02 -0700

    possibly fixed route?

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.forgot_password '/forgot_password', :controller => 'users', :action => 'forgot_password'
   map.connect '/send_reset_code', :controller => 'users', :action => 'send_reset_code'
-  map.connect '/reset_password', :controller => 'users', :action => 'reset_password'
+  map.connect '/reset_password/:reset_code', :controller => 'users', :action => 'reset_password'
   
   map.resources :users
 
